5b99a4e99c2f57ad7a7f76ccb175799094c2e747
equestriann <x.dormostuk@gmail.com>
2024-04-18 21:39:23 +0300

    swagger&redoc connected

--- pereval_project/pereval_project/urls.py
+++ pereval_project/pereval_project/urls.py
@@ -16,12 +16,31 @@ Including another URLconf
 """
 
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework import routers, permissions
 from pereval.views import *
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="FSTR Pass API",
+        default_version='v1',
+        description="API descriptiom",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny,],
+)
+
 
 router = routers.DefaultRouter()
 router.register(r'Pereval', PassViewset)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
